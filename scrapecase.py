d0588a725e86ead3169e6f0d6bb2a6c41c319177
Mathijs van Westendorp <mvwestendorp@gmail.com>
2018-01-23 18:07:44 +0100

    Rewrite in python for getting csv
    Compatible with crawler manager

--- scrapecase.py
+++ scrapecase.py
@@ -11,10 +11,17 @@ import sys
 from bs4 import BeautifulSoup
 import os
 import string
+import cProfile
 
-#TODO check why some webdrivers fail.
+import MySQLdb
+import re
+#import concurrent.futures
+from multiprocessing.dummy import Pool
 
-def scrapecases(appnos):
+
+def scrapecaselist(appnos):
+    pr = cProfile.Profile()
+    pr.enable()
     options= Options()    
     options.add_argument('-headless')
     driver = webdriver.Firefox(executable_path='geckodriver', firefox_options=options)
@@ -60,7 +67,41 @@ def scrapecases(appnos):
                 driver.quit() 
                 driver = webdriver.Firefox(executable_path='geckodriver', firefox_options=options)               
     #driver.quit()
+    pr.disable()
+    pr.print_stats(sort='time')
     print ("Quiting scrapecases function")
+
 def insert_slash(string, index):
     return string[:index] + '/' + string[index:]
+	
+def scrapecases():
+	connection = MySQLdb.connect(   host = "localhost",
+									user = "root",
+									passwd = "phpsuperw8",
+									db = "hudoc")
+	cursor = connection.cursor(nothreads = 1)
+	#TODO change sql to get only missing items
+	sql = """SELECT `Application Number` FROM `judgements` WHERE 1"""
+	cursor.execute(sql)
+	appnos = cursor.fetchall()
+	connection.commit()
+	cursor.close()
+	connection.close()
+	appnos = [appno for element in appnos for appno in element]
+	appnos = list(set(appnos)) #undouble
+	
+	#nothreads = 1
+	#if len(sys.argv) >= 2:
+	#	nothreads = int(sys.argv[1])
+
+	#sc.scrapecases(appnos)
+	#spawn threads and scrape
+	#executor = concurrent.futures.ProcessPoolExecutor(nothreads)
+	#futures = [executor.submit(sc.scrapecases, chunk) for chunk in chunks]
+	#concurrent.futures.wait(futures)
+
+	with Pool(processes=nothreads) as pool:
+		pool.map(scrapecaselist, [appnos[x:x+round(len(appnos)/nothreads)] for x in range(0, len(appnos), round(len(appnos)/nothreads))]) 
+		pool.close()
+		pool.join()
 
--- scrapehudoc.py
+++ scrapehudoc.py
@@ -1,44 +1,79 @@
-from selenium import webdriver
-import selenium
-from selenium.webdriver import Firefox
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support import expected_conditions as expected
-from selenium.webdriver.support.wait import WebDriverWait
+import pycurl
 import MySQLdb
 import re
-import sys
-import scrapecase as sc
-import concurrent.futures
-from multiprocessing.dummy import Pool
+import datetime
+import scrapecase
+import csv
+
+def read_file(filename):
+	with open(filename, mode='r') as infile:
+		#reader = csv.reader(infile)
+		#mydict = {rows[0]:rows[5] for rows in reader}
+		#return mydict
+		content = infile.readlines()
+		content = [x.strip() for x in content] 
+		return content
+
+#param format: '%Y-%m-%d'
+#cursor to database as cursor
+def getcaselist(fromdate,todate, cursor):
+	print("Parsing from date " + str(fromdate) + " to " + str(todate), end='\r')
+	url = 'https://hudoc.echr.coe.int/app/transform/csv?library=echreng&query=contentsitename%3AECHR%20AND%20(NOT%20(doctype%3DPR%20OR%20doctype%3DHFCOMOLD%20OR%20doctype%3DHECOMOLD))%20AND%20((documentcollectionid%3D%22GRANDCHAMBER%22)%20OR%20(documentcollectionid%3D%22CHAMBER%22))%20AND%20(kpdate%3E%3D%22'+fromdate+'T00%3A00%3A00.0Z%22%20AND%20kpdate%3C%3D%22'+todate+'T00%3A00%3A00.0Z%22)&sort=&start=0&length=500&rankingModelId=11111111-0000-0000-0000-000000000000'
+	#print(url)
+	## Parse csv into database
+	#post_data = ''
+	fp = open('temp_file.csv','wb')
+	#response = io.BytesIO()
+	c = pycurl.Curl()
+	c.setopt(c.URL, url)
+	#c.setopt(c.WRITEFUNCTION, response.write)
+	c.setopt(c.WRITEFUNCTION, fp.write)
+	#c.setopt(pycurl.VERBOSE, 1)
+	#c.setopt(c.HTTPHEADER, 0)
+	#c.setopt(c.POSTFIELDS, post_data)
+	c.perform()
+	
+	
+	#read downloaded csv file
+	cr_dict = read_file('temp_file.csv')
+	fp.close()
+	#if response > 500 entries, split date and recall function
+	# Check if file <501 lines
+	#print(len(cr_dict))
+	c.close()
+	cr_dict.pop(0) # remove row with column names
+	if len(cr_dict) <= 500:
+		for case in cr_dict:
+			cr = case.split(',')
+			#print(cr)
+			date = cr[4].split(" ")
+			print(cr[1])
+			sql = 'REPLACE INTO `judgements` (`Document Title`, `Application Number`, `Document Type`, `Originating Body`, `Date`, `Conclusion`) VALUES (%s,%s,%s,%s,%s,%s)';
+			cursor.execute(sql, (cr[0], cr[1].replace("/",""), cr[2], cr[3], datetime.datetime.strptime(date[0], "%d/%m/%Y"), cr[5]))
+	else:
+		#date diff /2 -> call function two times for lower and upper bound (threads?)
+		getcaselist(fromdate, fromdate + (todate-fromdate)/2 + 1)
+		getcaselist(fromdate + (todate-fromdate)/2, todate)
+	
 
 
-connection = MySQLdb.connect(   host = "localhost",
-                                user = "root",
-                                passwd = "phpsuperw8",
-                                db = "HUDOC")
-cursor = connection.cursor()
-#TODO change sql to get only missing items
-sql = """SELECT `Application Number` FROM `judgements` WHERE 1"""
-cursor.execute(sql)
-appnos = cursor.fetchall()
-connection.commit()
-cursor.close()
-connection.close()
-appnos = [appno for element in appnos for appno in element]
-appnos = list(set(appnos)) #undouble
-nothreads = 1
-if len(sys.argv) >= 2:
-    nothreads = int(sys.argv[1])
-
-#sc.scrapecases(appnos)
-#spawn threads and scrape
-#executor = concurrent.futures.ProcessPoolExecutor(nothreads)
-#futures = [executor.submit(sc.scrapecases, chunk) for chunk in chunks]
-#concurrent.futures.wait(futures)
-
-with Pool(processes=nothreads) as pool:
-    pool.map(sc.scrapecases, [appnos[x:x+round(len(appnos)/nothreads)] for x in range(0, len(appnos), round(len(appnos)/nothreads))]) 
-    pool.close()
-    pool.join()
+def crawl(lastsync):
+	### Establish connection to database
+	connection = MySQLdb.connect(   host = "localhost",
+									user = "root",
+									passwd = "phpsuperw8",
+									db = "hudoc")
+	connection.set_character_set('utf8')
+	
+	cursor = connection.cursor()
+	cursor.execute('SET NAMES utf8;')
+	cursor.execute('SET CHARACTER SET utf8;')
+	cursor.execute('SET character_set_connection=utf8;')
+	today = datetime.datetime.now().strftime('%Y-%m-%d')
+	getcaselist(lastsync,"2001-06-01",cursor)
+	connection.commit()
+	cursor.close()
+	connection.close()
+	return True
+	
+crawl("2001-05-01")
